b78a1b496041101818e96030fb829e8c51965ee0
xinyue Shi <sxyloading@gmail.com>
2013-11-21 22:14:18 +0800

    更改了测试的代码 执行多线程请求
    Signed-off-by: xinyue Shi <sxyloading@gmail.com>

--- TestSendAndRecieve/Form1.Designer.cs
+++ TestSendAndRecieve/Form1.Designer.cs
@@ -28,22 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(40, 81);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 21);
-            this.textBox1.TabIndex = 0;
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(192, 78);
+            this.button1.Location = new System.Drawing.Point(90, 100);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(111, 45);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -55,17 +47,14 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
     }
 }
--- TestSendAndRecieve/Form1.cs
+++ TestSendAndRecieve/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace TestSendAndRecieve
 {
@@ -18,18 +19,46 @@ namespace TestSendAndRecieve
 
         private void button1_Click(object sender, EventArgs e)
         {
+            int cases = 1000;
+            for (int i = 0; i < cases; i++)
+            {
+                Thread t = new Thread(new Test().TestSample);
+                t.Start();
+            }
+        }
+    }
+    class Test
+    {
+        private static Random rand = new Random();
+        private static object Lock = new object();
+        public static int success = 0;
+        public Test()
+        {
+        }
+        public void TestSample()
+        {
+            int limit = 9999;
             TaskProcedure tp = new TaskProcedure();
-            string s = textBox1.Text;
+            string s = rand.Next(limit).ToString();
             byte[] b = UTF8Encoding.UTF8.GetBytes(s);
             byte[] result = tp.GetResult(b);
             if (result == null)
             {
-                Console.WriteLine("fail");
+                Console.WriteLine(s + " Fail");
             }
             else
             {
-                Console.WriteLine(UTF8Encoding.UTF8.GetString(result, 0, result.Length));
+                Console.WriteLine(s+" "+UTF8Encoding.UTF8.GetString(result, 0, result.Length));
+                if (s.Equals(UTF8Encoding.UTF8.GetString(result, 0, result.Length)))
+                {
+                    lock (Lock)
+                    {
+                        success++;
+                    }
+                }
+                Console.WriteLine("total success "+success);
             }
+            
         }
     }
 }
